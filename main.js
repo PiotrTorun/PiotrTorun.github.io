f7e7c741ce0a91419193a78c3567196d01fe8e7f
PiotrTorun <63645781+PiotrTorun@users.noreply.github.com>
2020-04-14 21:00:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-var para = document.querySelector('p');
-para.addEventListener('click', updateName);
-function updateName() {
-  var name = prompt('Enter a new name');
-  para.textContent = 'Player 1: ' + name;
+function stworzParagraf() {
+  var para = document.createElement('p');
+  para.textContent = 'Kliknąłeś przycisk!';
+  document.body.appendChild(para);
+}
+
+var przyciski = document.querySelectorAll('button');
+
+for (var i = 0; i < przyciski.length ; i++) {
+  przyciski[i].addEventListener('click', stworzParagraf);
 }
