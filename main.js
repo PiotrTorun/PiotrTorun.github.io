abc9523fdd63fe9de2dc2140ef895375ce14e5d6
PiotrTorun <63645781+PiotrTorun@users.noreply.github.com>
2020-04-14 22:24:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var para = document.querySelector('p');
+let para = document.querySelector('p');
 
 para.addEventListener('click', updateName);
 
 function updateName() {
-  var name = prompt('Enter a new name');
+  let name = prompt('Enter a new name');
   para.textContent = 'Player 1: ' + name;
 }
